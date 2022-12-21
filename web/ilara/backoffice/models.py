5dc9b9036a04900b45738d1ebcc56b9aa314c478
jimmykamau <jimmykambiz@gmail.com>
2022-12-21 16:41:16 +0300

    Create UserProfile model

---
+++ web/ilara/backoffice/models.py
@@ -0,0 +1,28 @@
+from django.conf import settings
+from django.db import models
+from django.utils.translation import gettext as _
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+class UserProfile(models.Model):
+    GENDER_MALE = 1
+    GENDER_FEMALE = 2
+    GENDER_CHOICES = [
+        (GENDER_MALE, _("Male")),
+        (GENDER_FEMALE, _("Female")),
+    ]
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    birthday = models.DateField(null=True, blank=True)
+    gender = models.PositiveSmallIntegerField(
+        choices=GENDER_CHOICES, null=True, blank=True
+    )
+    phone = PhoneNumberField(blank=True)
+    address = models.CharField(max_length=255, null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = _("User Profile")
+        verbose_name_plural = _("User Profiles")
